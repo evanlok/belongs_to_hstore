31e4d8d16468c7e24c41ce7ca9099641b42f9a9b
Evan Lok <elok45@gmail.com>
2013-10-27 19:49:12 -0700

    Fix initializer

--- lib/belongs_to_hstore.rb
+++ lib/belongs_to_hstore.rb
@@ -3,9 +3,11 @@ require 'belongs_to_hstore/hstore_query_helper'
 require 'belongs_to_hstore/association'
 
 module BelongsToHstore
-  initializer "belongs_to_hstore.active_record" do
-    ActiveSupport.on_load :active_record do
-      ActiveRecord::Base.include BelongsToHstore::Association
+  class Railtie < Rails::Railtie
+    initializer "belongs_to_hstore.active_record" do
+      ActiveSupport.on_load :active_record do
+        ActiveRecord::Base.include BelongsToHstore::Association
+      end
     end
   end
 end
