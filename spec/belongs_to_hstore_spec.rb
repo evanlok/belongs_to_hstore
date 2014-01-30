43c7f9fc3b5807467d83a10341a8113d2e1691ac
Dave Gynn <dgynn@unow.com>
2014-01-29 23:06:15 -0800

    use a class attribute to store valid attribute names for faster lookup this approach initializes the attribute array at class definition time and reduces parent class lookups

--- spec/belongs_to_hstore_spec.rb
+++ spec/belongs_to_hstore_spec.rb
@@ -9,12 +9,17 @@ class Widget < ActiveRecord::Base
   belongs_to_hstore :properties, :poly_item, :polymorphic => true
 end
 
+class ExtendedWidget < Widget
+  belongs_to_hstore :properties, :additonal_item, :class_name => 'Item'
+end
+
 class Item < ActiveRecord::Base
 end
 
 describe BelongsToHstore do
   let(:item) { Item.create }
   let(:widget) { Widget.new }
+  let(:extended_widget) { ExtendedWidget.new }
 
   it 'sets properties from object' do
     widget.item = item
@@ -45,4 +50,28 @@ describe BelongsToHstore do
       widget.reload.poly_item.class.should == item.class
     end
   end
+
+  context 'subclasses' do
+    it 'sets/gets properties from base class' do
+      extended_widget.item = item
+      extended_widget.poly_item = item
+      extended_widget.properties['item_id'].should == item.id.to_s
+      extended_widget.properties['poly_item_id'].should == item.id.to_s
+      extended_widget.properties['poly_item_type'].should == item.class.to_s
+    end
+
+    it 'sets/gets properties from subclass' do
+      extended_widget.additonal_item = item
+      extended_widget.properties['additonal_item_id'].should == item.id.to_s
+      extended_widget.additonal_item.should == item
+    end
+
+    it 'does not add subclass properties to base class' do
+      expect{widget.additonal_item = item}.to raise_error(NameError)
+      expect{widget.additonal_item}.to raise_error(NameError)
+      Widget.belongs_to_hstore_attributes.should include('item_id')
+      Widget.belongs_to_hstore_attributes.should_not include('additional_item_id')
+    end
+  end
+
 end
